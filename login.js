2e368064c48f89aad53bbc1365d7792e60ad030a
AhmetBozkurt1 <ahmet_bozkurt812@outlook.com>
2023-09-14 20:57:50 +0300

    signUp giriş işlemleri yapıldı

--- login.js
+++ login.js
@@ -30,3 +30,55 @@ formIconEye2.addEventListener("click",function(){
     }
 })
 
+const signUpButton=document.querySelector(".signUp-button")
+
+signUpButton.addEventListener("click",function(){
+    const signUpName=document.querySelector(".signUp-formName").value
+    const signUpEmail=document.querySelector(".signUp-formEmail").value
+    const signUpPass=document.querySelector(".signUp-formPass").value
+    const signUpPass2=document.querySelector(".signUp-formPass2").value
+    let signUpMessage=document.querySelector(".signUp-textMessage")
+
+    let kullanicilar=JSON.parse(localStorage.getItem("kullanicilar")) || []
+
+    let varUser=false
+
+    for (const element of kullanicilar) {
+        if(signUpName==element.kadi && signUpEmail==element.email && signUpPass==element.sifre && signUpPass2==element.sifreTekrar){
+            varUser=true
+            break
+        }
+    }
+
+    if(varUser!=true){
+        if(signUpName.length>0 && signUpEmail.length>0){
+            if(signUpPass.length>6 && signUpPass2.length>6){
+                if(signUpPass==signUpPass2){
+                    kullanicilar.push(
+                        {
+                            kadi:signUpName,
+                            email:signUpEmail,
+                            sifre:signUpPass,
+                            sifreTekrar:signUpPass2
+                        }
+                    )
+                    localStorage.setItem("kullanicilar",JSON.stringify(kullanicilar))
+                    let goAccount="account.html"
+                    signUpMessage.innerHTML=`Registration Successful <a id="accountGo"
+                    href="${goAccount}">Sign In</a>`
+                }else{
+                    signUpMessage.innerHTML=`Try Again With Different Passwords`
+                }
+            }else{
+                signUpMessage.innerHTML=`Password Cannot Be Less Than 6 Characters`
+            }
+        }else{
+            signUpMessage.innerHTML=`Name And Email Fields Cannot Be Left Blank.`
+        }
+    }else{
+        let goAccount="account.html"
+        signUpMessage.innerHTML=`You Have a Membership Registration <a id="accountGo"
+        href="${goAccount}">Sign In</a>`
+    }
+})
+
